6f36ff252e5f0119db1192113c751dc3ddba8d30
allanSantos <allande1994@hotmail.com>
2021-10-30 08:54:18 -0300

    finalizando classes trabalhando com datas

--- Trabalhando-com-datas/src/br/com/allan/datas/DatasNoJava8.java
+++ Trabalhando-com-datas/src/br/com/allan/datas/DatasNoJava8.java
@@ -1,19 +1,31 @@
 package br.com.allan.datas;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class DatasNoJava8 {
-	
+
 	public static void main(String[] args) {
-		
+
 		LocalDate agora = LocalDate.now();
 		System.out.println(agora);
-		
+
 		LocalDate ontem = agora.minusDays(1);
 		System.out.println(ontem);
-		
-		
-		
+
+		LocalTime hoje = LocalTime.now();
+
+		System.out.println(hoje);
+
+		LocalTime maisUmaHora = hoje.plusHours(1);
+
+		System.out.println(maisUmaHora);
+
+		LocalDateTime agoraHoje = LocalDateTime.now();
+		LocalDateTime futuro = agoraHoje.plusDays(1).plusHours(2);
+		System.out.println(futuro);
+
 	}
 
 }
